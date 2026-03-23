9526596adb75dcb19a48217ae7ba2032b5c6c775
Parth Gajjar <parthgajjar127@gmail.com>
2026-03-23 23:26:53 +0530

    feat: toast msg

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,26 @@ import Footer from './components/Footer'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import CustomCursor from './utils/CursorAnimation'
+import { Toaster } from "react-hot-toast"
 
 export default function App() {
   return (
     <div className='font-sora scroll-smooth overflow-x-hidden'>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#111",
+            color: "#fff",
+            borderRadius: "12px",
+            padding: "12px 16px",
+            fontSize: "14px",
+          },
+        }}
+      />
+
       <CustomCursor/>
       <Navbar />
       <Home />
@@ -18,7 +34,6 @@ export default function App() {
       <About />
       <Projects />
       <Contact />
-
       <Footer />
     </div>
   )
